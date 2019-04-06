b67bb4f96c6d30445d1be89e101847bc3bb0f536
Ansil H <ansilh@gmail.com>
2019-04-07 03:18:34 +0530

    Updated to v.17

--- 12-ingress-controller.sh
+++ 12-ingress-controller.sh
@@ -9,14 +9,14 @@
 #----------------------------------------------------
 
 # Apply RBAC rules for traefik
-kubectl apply -f https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/traefik-rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/traefik-rbac.yaml
 
 # Download deployment yaml and change service IP type to LoadBalancer
-wget -q https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/traefik-deployment.yaml
+wget -q https://github.com/containous/traefik/blob/v1.7/examples/k8s/traefik-deployment.yaml
 sed -i 's/type: NodePort/type: LoadBalancer/' traefik-deployment.yaml
 kubectl apply -f traefik-deployment.yaml
 
 # Download Traefik UI yaml and change the virtual hostname from default to traefik-ui.linxlabs.local 
-wget -q https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/ui.yaml
+wget -q https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/ui.yaml
 sed -i 's/traefik-ui.minikube/traefik-ui.linxlabs.local/' ui.yaml
 kubectl apply -f ui.yaml
