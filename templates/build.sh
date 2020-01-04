14b40de6dea705f66c94a3646a92b95defdbdbd7
Ansil H <ansilh@gmail.com>
2020-01-05 01:18:09 +0530

    README templating change

--- templates/build.sh
+++ templates/build.sh
@@ -5,7 +5,21 @@
 # READ VERSION FILE
 # GET EACH WORD FROM VERSION FILE
 # REPLACE THE WORD WITH THE VALUE
+DEV_HEADER_MSG="may be in the middle of a change - use with caution"
+PROD_HEADER_MSG=""
+ICON_DEV=":fire:"
+ICON_PROD=":white_check_mark:"
+
 BRANCH=$(basename $(cat ../.git/HEAD|awk '{print $2}'))
+if [ "${BRANCH}" == "development" ]
+then
+    ICON=${ICON_DEV}
+    MSG=${DEV_HEADER_MSG}
+else
+    ICON=${ICON_PROD}
+    MSG=${PROD_HEADER_MSG}
+fi
+
 echo ${BRANCH}
 cp  README.template ../README.md
 cp  Vagrantfile.template ../Vagrantfile
@@ -20,3 +34,5 @@ do
     sed -i "s/{{${WORD}}}/${VALUE}/g" ../README.md
     sed -i "s/{{${WORD}}}/${VALUE}/g" ../Vagrantfile
 done
+sed -i "s/{{MSG}}/${MSG}/g" ../README.md
+sed -i "s/{{ICON}}/${ICON}/g" ../README.md
